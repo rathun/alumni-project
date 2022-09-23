4d202cab0b0b145edbabb19fc12816bebc9dbb4c
rathun <rathun.17it@kongu.edu>
2022-09-23 20:24:20 +0530

    alumni update

--- alumni_feedback_form.php
+++ alumni_feedback_form.php
@@ -8,6 +8,7 @@
 <html>
 <head>
 	<title>Feedback</title>
+	//demo
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=yes" />
 	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
